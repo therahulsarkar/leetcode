b406ca343c09d7681ba78a92de0781e1f56d95fe
Rahul <64316818+therahulsarkar@users.noreply.github.com>
2023-11-23 21:33:44 +0530

    Time: 78 ms (28.66%) | Memory: 51.8 MB (19.82%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -2,16 +2,32 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    let current = prices[0];
-    let maxProfit = 0;
+// var maxProfit = function (prices) {
+//     let buy = prices[0];
+//     let maxProfit = 0;
 
-    for(let i=0; i<prices.length; i++){
-       if(prices[i] > current){
-           maxProfit = Math.max(maxProfit, prices[i]-current);
-       } else{
-           current = prices[i]
-       }
+//     for (let i = 1; i < prices.length; i++) {
+//         //If price today is more than our buying price then sell and compute max profit 
+//         if (prices[i] > buy) { 
+//             maxProfit = Math.max(maxProfit, prices[i] - buy);
+//         } else {
+//             //If not, that means stock price today is less than our previous buying price, so now buy today
+//             buy = prices[i];
+//         }
+//     }
+//     return maxProfit;
+// };
+
+var maxProfit = function (prices) {
+    let buy = prices[0];
+    let maxProfit = 0;
+    let profit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        profit = prices[i] - buy;
+        maxProfit = Math.max(profit, maxProfit);
+        buy = Math.min(buy, prices[i]);
     }
     return maxProfit;
 };
+
