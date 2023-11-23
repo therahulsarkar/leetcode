c431a6654da6f662314610ee24b256a7e412d9a6
Rahul <64316818+therahulsarkar@users.noreply.github.com>
2023-11-23 21:47:55 +0530

    Time: 334 ms (8.40%) | Memory: 95.3 MB (66.67%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    let posIndex = 0;
+    let negIndex = 1;
+    let newArr = [];
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i]< 0){
+            newArr[negIndex] = nums[i];
+            negIndex +=2;
+        }else{
+            newArr[posIndex] = nums[i];
+            posIndex +=2;
+        }
+    }
+
+    return newArr;
+};
