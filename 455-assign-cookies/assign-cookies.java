a0ea9cddbf60b531f06f1f94e405dff369b72948
Rahul <64316818+therahulsarkar@users.noreply.github.com>
2024-01-01 15:25:14 +0530

    Time: 11 ms (11.23%) | Memory: 45.4 MB (6.13%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
 
+        // Sorting the arrays to simplify the allocation process
         Arrays.sort(g);
         Arrays.sort(s);
         int count = 0;
@@ -9,15 +12,17 @@ class Solution {
         int childIndex = 0;
 
         while(childIndex < g.length && cookieIndex < s.length){
+            // If the current child can be satisfied with the current cookie
             if(g[childIndex] <= s[cookieIndex]){
                 count++;
+                // Moving to the next child and cookie
                 cookieIndex++;
                 childIndex++;
-            } else{
+            } else {
+                // If the current cookie is not enough for the current child, move to the next cookie bcz next cookie is bigger in size 
                 cookieIndex++;
             }
         }
-
         return count;
     }
 }
