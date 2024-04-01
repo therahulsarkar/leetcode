1a1a306b3baa3d24f6390cb3b38f137751d62913
Rahul <64316818+therahulsarkar@users.noreply.github.com>
2024-04-02 01:29:39 +0530

    Time: 58 ms (19.07%) | Memory: 49 MB (25.19%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let count = 0;
+    for(let i=s.length-1; i>=0; i--){
+        if(s[i] != ' ') ++count;
+        else if(s[i] === ' ' &&  count)break;}
+    return count;
+};
