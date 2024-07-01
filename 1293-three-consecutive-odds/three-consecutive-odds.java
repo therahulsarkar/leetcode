3aa3f30a3fa15853440f72f38d57c958625b3c8e
Rahul <64316818+therahulsarkar@users.noreply.github.com>
2024-07-01 22:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (86.80%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,28 +1,34 @@
-class Solution {
-    
-    boolean isOdd(int a, int b, int c){
-        if (a % 2 == 0) return false;        
-        else if (b % 2 == 0) return false;
-        else if (c % 2 == 0) return false;
-        else return true;
+// class Solution {
 
-    }
-    
+//     boolean isOdd(int a, int b, int c){
+//         if (a % 2 == 0) return false;        
+//         else if (b % 2 == 0) return false;
+//         else if (c % 2 == 0) return false;
+//         else return true;
+
+//     }
+
+//     public boolean threeConsecutiveOdds(int[] arr) {
+//         int length = arr.length;
+//         int count = 0;
+
+//         for(int i = 0; i<length-2;i++){
+//            if(isOdd(arr[i], arr[i+1], arr[i+2])){
+//                 count++;
+//            }
+//         }
+
+//         return count > 0;
+//     }
+// }
+
+class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        int length = arr.length;
-        int count = 0;
-        
-        for(int i = 0; i<length-2;i++){
-           if(isOdd(arr[i], arr[i+1], arr[i+2])){
-                count++;
-           }
+    for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1 ) {
+                return true;
+            }
         }
-        
-        return count > 0;
+        return false;
     }
 }
-
-
-
-    
-    
