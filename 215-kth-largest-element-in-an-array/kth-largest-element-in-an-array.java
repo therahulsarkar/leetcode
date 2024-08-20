423451b980fd8bc1d0b35478b391a7f2912c71a1
Rahul <64316818+therahulsarkar@users.noreply.github.com>
2024-08-20 20:44:59 +0530

    Time: 39 ms (72.78%) | Memory: 56.9 MB (91.03%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,14 +1,35 @@
 import java.util.PriorityQueue;
 
+// class Solution {
+
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(k + 1); //Creating a queue of size k+1 to store the elements in min heap
+
+//         for (int i = 0; i < k; i++) {
+//             pq.add(nums[i]); //Adding k elements in the priority queue
+//         }
+
+//         for (int i = k; i < nums.length; i++) { //Going through the other elements
+//             if (pq.peek() < nums[i]) { //If the top elemenet of the pq is smaller than the current element then remove the top element and add the current element
+//                 pq.poll();
+//                 pq.add(nums[i]);
+//             }
+//         }
+
+//         return pq.peek(); //Return the top element
+//     }
+// }
+
 class Solution {
+
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(k+1);
 
-        for(int i = 0; i<k; i++){
+        for(int i=0; i<k; i++){
             pq.add(nums[i]);
         }
 
-        for(int i = k; i<nums.length; i++){
+        for(int i=k; i<nums.length; i++){
             if(pq.peek() < nums[i]){
                 pq.poll();
                 pq.add(nums[i]);
