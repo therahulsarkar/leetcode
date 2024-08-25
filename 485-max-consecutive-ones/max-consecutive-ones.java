19c22a5550693ebb57eb401ab4d2d3bbbfdea39f
Rahul <64316818+therahulsarkar@users.noreply.github.com>
2024-08-25 21:34:27 +0530

    Time: 3 ms (48.04%) | Memory: 50.1 MB (7.11%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -3,14 +3,10 @@ class Solution {
         int right = 0, left = 0, maxLength = 0, ones = 0;
 
        while(right < nums.length){
-        
-        //To shift left pointer
         if(nums[right] == 0){
             left = right + 1;
         }
-
         maxLength = Math.max(maxLength, right-left+1);
-        
         right++;
        }
 
