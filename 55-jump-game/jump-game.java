9effdb5fbf888b93b82aa96d16c724d80017a149
Rahul <64316818+therahulsarkar@users.noreply.github.com>
2024-09-18 17:51:36 +0530

    Time: 3 ms (49.30%) | Memory: 45.6 MB (43.55%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -3,11 +3,11 @@ class Solution {
         int currMaxPossibleIndex = 0;
 
         for(int i = 0; i<nums.length; i++){
-            if(i + nums[i] > currMaxPossibleIndex && i <= currMaxPossibleIndex){
-                currMaxPossibleIndex = i + nums[i];
-            } else if(i > currMaxPossibleIndex){
+            if(i > currMaxPossibleIndex){
                 return false;
-            }
+            }else if (i + nums[i] > currMaxPossibleIndex && i <= currMaxPossibleIndex){
+                currMaxPossibleIndex = i + nums[i];
+            } 
         }
 
         return true;
