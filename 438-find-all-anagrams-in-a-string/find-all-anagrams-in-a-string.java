f7c85d3a1ec20c95cf9775d38b6b71b1bebca190
Rahul <64316818+therahulsarkar@users.noreply.github.com>
2024-10-26 03:13:17 +0530

    Time: 10 ms (79.61%) | Memory: 44.2 MB (98.94%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+
+        if (p.length() > s.length())
+            return res;
+
+        int[] freq_p = new int[26];
+        int[] freq_s = new int[26];
+
+        // Freq of each char
+        for (int i = 0; i < p.length(); i++) {
+            freq_p[p.charAt(i) - 'a']++;
+        }
+
+        // window in string2 of size string1
+        for (int j = 0; j < p.length(); j++) {
+            freq_s[s.charAt(j) - 'a']++;
+        }
+
+        if (isSame(freq_p, freq_s))
+            res.add(0);
+
+        // Left and right pointer for window
+        int right = p.length();
+        int left = 0;
+
+        while (right < s.length()) {
+            freq_s[s.charAt(right) - 'a']++; // Dec the freq of new char
+            freq_s[s.charAt(left) - 'a']--; // Inc the freq of prev char
+            if (isSame(freq_p, freq_s)) {
+                res.add(left + 1);
+            }
+            // Move both the pointers
+            right++;
+            left++;
+        }
+        return res;
+    }
+
+    public boolean isSame(int[] freq1, int[] freq2) {
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i])
+                return false;
+        }
+        return true;
+    }
+}
